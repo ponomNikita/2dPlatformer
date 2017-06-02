c543c345378152d9312140d7e332cf042ebc6668
Пономарев Никита <nikita.ponomarev@norbit.ru>
2017-06-02 22:27:50 +0300

    Initial commit

---
+++ Platformer/Assets/Scripts/BaseController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+	public class BaseController
+	{
+        private Animator _animatior;
+
+        private Rigidbody2D _hero;
+
+        private Transform _transform;
+
+        private bool isFaceRight;
+
+
+        public BaseController (Transform transform, Rigidbody2D hero, Animator animatior)
+		{
+			_hero = hero;
+            _animatior = animatior;
+            isFaceRight = true;
+            _transform = transform;
+        }
+
+		public void Move(float horizontal, float speed)
+		{
+            _animatior.SetFloat("speed", Mathf.Abs(horizontal));
+            _hero.velocity = new Vector2(horizontal * speed, _hero.velocity.y);
+            Flip(horizontal);
+        }
+
+        private void Flip(float horizontal)
+        {
+            if (isFaceRight && horizontal < 0 || !isFaceRight && horizontal > 0)
+            {
+                isFaceRight = !isFaceRight;
+                Vector2 scale = _transform.localScale;
+                scale.x *= -1;
+                _transform.localScale = scale;
+            }
+        }
+    }
+}
+
---
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private BaseController _baseController;
+
+        private Animator _animatior;
+
+        private Rigidbody2D _player;
+
+        [SerializeField]
+        private float _speed;
+
+        void Start()
+        {
+            _player = GetComponent<Rigidbody2D>();
+            _animatior = GetComponent<Animator>();
+            _baseController = new BaseController(transform, _player, _animatior);
+        }
+
+        void Update()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            _baseController.Move(horizontal, _speed);
+        }
+    }
+}
